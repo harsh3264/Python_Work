ee3b73b2930cc4d91212bde161ab4d07693300a1
Harsh Raval <harshraval3264@gmail.com>
2016-10-21 13:18:27 +0530

    bug_fix_1

--- learn.py
+++ learn.py
@@ -1,14 +1,15 @@
 import warnings
 warnings.filterwarnings("ignore") # To remove all the warnings
-
 import pandas as pd # Pandas library call
 import numpy as np # numpy lib
 import seaborn as sns # seaborn module
 import matplotlib.pyplot as plt # generate the graph
 from sklearn.cross_validation import train_test_split # Machine learning came into the picture scikit library
 from sklearn.linear_model import LinearRegression # Import linear regression
+from sklearn import metrics
+from math import sqrt  # import metrics to calculate the Root mean square Error (RMSE) value
 
-data = pd.read_csv('demo.csv') # panda will read the csv file
+data = pd.read_csv('names.csv') # panda will read the csv file
 data.index = np.arange(1,len(data)+1) # set the index as 1, if column 1 of data set is index then set index_col = 0 
 
 feature_cols = ['Sales','ABS','Customers'] # Create a list which contains all the required X axis values
@@ -16,14 +17,9 @@ feature_cols = ['Sales','ABS','Customers'] # Create a list which contains all th
 X = data[feature_cols]
 y = data.ATV # Y axis only contains one value and it is an int that's why direct data.sales
 
-
-
-
 X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=1) # split up the data set into training and testing data 
 
 # To check the splits use print X_train.shape, print y_test.shape. Shape is used to check the split. By default split is 75 to 25
-
-
 
 linreg = LinearRegression() # Instantiate the model
 
@@ -44,7 +40,6 @@ print zip(feature_cols, linreg.coef_) # For each value know the coef
 
 y_pred = linreg.predict(X_test) # Make prediction on the testing test
 
-from sklearn import metrics # import metrics to calculate the Root mean square Error (RMSE) value  
 print np.sqrt(metrics.mean_squared_error(y_test,y_pred)) # RMSE value for the test set and the predicted value
 
 # Output is 22.14 which means this method has not useful featured column. 
